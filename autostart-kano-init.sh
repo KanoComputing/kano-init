a0b40669a3411a8d09fffa96397f1967e7b7c4dd
asimon <alejandro@kano.me>
2013-12-02 20:54:36 +0000

    Added License, bumped version and removed subshellrc

--- autostart-kano-init.sh
+++ autostart-kano-init.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
+# autostart-kano-init.sh
+#
+# Copyright (C) 2013 Kano Computing Ltd.
+# License:   http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+#
 # 0: Disabled
 # 1: Name
-# 2: Rabbit
-# 3: Bomb
+# 2: Email
+# 3: Rabbit
+# 4: Bomb
+
 STAGE=0
 
 if [ `id -u` -eq 0 -a "$STAGE" -gt 0 ]; then
