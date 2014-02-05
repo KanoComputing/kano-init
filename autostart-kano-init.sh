61d69b95bc42f596b66b5db2b6e140f87d86ba61
Zsolt Ero <zsolt.ero@gmail.com>
2014-02-05 14:30:09 +0000

    kano-init-multi

--- autostart-kano-init.sh
+++ autostart-kano-init.sh
@@ -2,15 +2,14 @@
 
 # autostart-kano-init.sh
 #
-# Copyright (C) 2013 Kano Computing Ltd.
+# Copyright (C) 2014 Kano Computing Ltd.
 # License:   http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
 #
 # 0: Disabled
 # 1: Name
-# 2: Country
-# 3: Email
-# 4: Rabbit
-# 5: Bomb
+# 2: Email
+# 4: Riddle
+# 9: Reset
 
 STAGE=0
 
@@ -18,3 +17,5 @@ if [ `id -u` -eq 0 -a "$STAGE" -gt 0 ]; then
     kano-init "$STAGE"
     kill -HUP $PPID
 fi
+
+
