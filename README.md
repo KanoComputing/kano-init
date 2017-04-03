065fee7ba84aaf5efb72ab71460c1a0e314f4df8
Albert Casals <skarbat@gmail.com>
2017-04-03 18:48:53 +0000

    Cleanup code from systemd conditionals

--- README.md
+++ README.md
@@ -4,10 +4,12 @@
 on a Kano kit. It is based mostly on the [raspi-config](https://github.com/asb/raspi-config)
 tool from Alex Bradbury.
 
-## New onboarding
+**kano-init** needs a Debian Jessie system running `systemd`.
+
+## Onboarding UI
 
 As of kano-init 3.3, there is a new version which uses a QML based graphical UI called Overture.
-It is bound to the system via systemd, the command will skip kano-init and take the kit
+It is bound to the system via systemd - the command below will skip kano-init and take the kit
 directly to the Dashboard or Greeter:
 
 ```
--- kano_init/utils.py
+++ kano_init/utils.py
@@ -13,7 +13,7 @@ import json
 import pwd
 import grp
 
-from kano.utils import sed, run_cmd, is_systemd
+from kano.utils import sed, run_cmd
 
 from kano_init.paths import INIT_CONF_PATH
 from kano_init.user import get_group_members
@@ -25,95 +25,87 @@ def enable_console_autologin(username, restart=False):
     Sets the system to automatically login username on tty1
     at boot time, or when you close the console session.
     '''
-    if is_systemd():
-        #
-        # Change systemd symlink that says what needs to happen on tty1
-        # https://wiki.archlinux.org/index.php/Systemd_FAQ#How_do_I_change_the_default_number_of_gettys.3F
-        #
-        systemd_tty1_linkfile = '/etc/systemd/system/getty.target.wants/getty@tty1.service'
-        kano_init_tty1_kanoautologin = '/usr/share/kano-init/systemd_ttys/kanoautologin@.service'
-        kano_init_tty1_kanoinit = '/usr/share/kano-init/systemd_ttys/kanoinit@.service'
 
-        if os.path.isfile(systemd_tty1_linkfile):
-            os.unlink(systemd_tty1_linkfile)
+    #
+    # Change systemd symlink that points to what needs to happen on tty1
+    # https://wiki.archlinux.org/index.php/Systemd_FAQ#How_do_I_change_the_default_number_of_gettys.3F
+    #
+    systemd_tty1_linkfile = '/etc/systemd/system/getty.target.wants/getty@tty1.service'
+    kano_init_tty1_kanoautologin = '/usr/share/kano-init/systemd_ttys/kanoautologin@.service'
+    kano_init_tty1_kanoinit = '/usr/share/kano-init/systemd_ttys/kanoinit@.service'
+    if os.path.isfile(systemd_tty1_linkfile):
+        os.unlink(systemd_tty1_linkfile)
 
-        if username == 'root':
-            os.symlink(kano_init_tty1_kanoinit, systemd_tty1_linkfile)
-        else:
-            sed('^ExecStart.*', "ExecStart=/bin/su - {}".format(username), kano_init_tty1_kanoautologin)
-            os.symlink(kano_init_tty1_kanoautologin, systemd_tty1_linkfile)
-
-        if restart:
-            # replace the tty process inmediately, otherwise on next boot
-            run_cmd('systemctl restart getty@tty1.service')
+    if username == 'root':
+        os.symlink(kano_init_tty1_kanoinit, systemd_tty1_linkfile)
     else:
-        sed('^(1:2345:respawn:/sbin/a?getty).+',
-            "\\1 -n -o\'-f {}\' 38400 tty1".format(username),
-            '/etc/inittab')
-        run_cmd('init q')
+        sed('^ExecStart.*', "ExecStart=/bin/su - {}".format(username), kano_init_tty1_kanoautologin)
+        os.symlink(kano_init_tty1_kanoautologin, systemd_tty1_linkfile)
+
+    if restart:
+        # replace the tty process immediately, otherwise on next boot
+        run_cmd('systemctl restart getty@tty1.service')
 
 
 def disable_console_autologin(restart=False):
     '''
     Disable automatic login on tty1, default getty login prompt will be provided.
     '''
-    if is_systemd():
-        #
-        # Change systemd symlink that says what needs to happen on tty1
-        # https://wiki.archlinux.org/index.php/Systemd_FAQ#How_do_I_change_the_default_number_of_gettys.3F
-        #
-        systemd_tty1_linkfile = '/etc/systemd/system/getty.target.wants/getty@tty1.service'
-        systemd_tty1_getty = '/lib/systemd/system/getty@.service'
 
-        if os.path.isfile(systemd_tty1_linkfile):
-            os.unlink(systemd_tty1_linkfile)
+    #
+    # Change systemd symlink that points to what needs to happen on tty1
+    # https://wiki.archlinux.org/index.php/Systemd_FAQ#How_do_I_change_the_default_number_of_gettys.3F
+    #
+    systemd_tty1_linkfile = '/etc/systemd/system/getty.target.wants/getty@tty1.service'
+    systemd_tty1_getty = '/lib/systemd/system/getty@.service'
 
-        os.symlink(systemd_tty1_getty, systemd_tty1_linkfile)
+    if os.path.isfile(systemd_tty1_linkfile):
+        os.unlink(systemd_tty1_linkfile)
 
-        if restart:
-            run_cmd('systemctl restart getty@tty1.service')
-    else:
-        sed('^(1:2345:respawn:/sbin/a?getty).+', '\\1 38400 tty1', '/etc/inittab')
-        run_cmd('init q')
+    os.symlink(systemd_tty1_getty, systemd_tty1_linkfile)
+
+    if restart:
+        run_cmd('systemctl restart getty@tty1.service')
 
 
 def set_ldm_autologin(username):
-    if is_systemd():
-        sed('^#?(autologin-user)=.*$', 'autologin-user={}'.format(username), '/etc/lightdm/lightdm.conf')
-    else:
-        ldm_conf_util = '/usr/lib/arm-linux-gnueabihf/lightdm/lightdm-set-defaults'
-        run_cmd("{} --autologin {}".format(ldm_conf_util, username))
+    '''
+    Tell lightdm to skip the greeter, login username, and go directly to the desktop.
+    '''
+    sed('^#?(autologin-user)=.*$', 'autologin-user={}'.format(username), '/etc/lightdm/lightdm.conf')
 
     # Make sure the autologin timeout is set to 0
     sed('^#?(autologin-user-timeout)=.*$', '\\1=0', '/etc/lightdm/lightdm.conf')
 
 
 def unset_ldm_autologin():
-    if is_systemd():
-        sed('^autologin-user=.*$', '#autologin-user=none', '/etc/lightdm/lightdm.conf')
-    else:
-        sed('^autologin-user=.*$', '', '/etc/lightdm/lightdm.conf')
+    '''
+    Disable lightdm automatic login, the greeter should provide a login window
+    '''
+    sed('^autologin-user=.*$', '#autologin-user=none', '/etc/lightdm/lightdm.conf')
 
     # Comment out the autologin-user-timeout option
     sed('^#?(autologin-user-timeout=.*)$', '#\\1', '/etc/lightdm/lightdm.conf')
 
 
 def enable_ldm_autostart():
-    if is_systemd():
-        run_cmd('systemctl set-default graphical.target')
-    else:
-        run_cmd('update-rc.d lightdm enable 2')
+    '''
+    Set the system to graphical mode - start the default X server and go to Desktop
+    '''
+    run_cmd('systemctl set-default graphical.target')
 
 
 def disable_ldm_autostart():
-    if is_systemd():
-        run_cmd('systemctl set-default multi-user.target')
-    else:
-        run_cmd('update-rc.d lightdm disable 2')
+    '''
+    Set the system to multi user mode - the X server will not be started,
+    and the Overture onboarding will take control, through the systemd service.
+    '''
+    run_cmd('systemctl set-default multi-user.target')
 
 
 def set_dashboard_onboarding(username, run_it=True):
     '''
+    FIXME: This onboarding stage is to be superseded by Overture
     Tells the Dashboard supervisor whether the Onboarding needs to take place
     '''
     onboarding_file=os.path.join('/home/{}'.format(username),
@@ -135,6 +127,12 @@ def set_dashboard_onboarding(username, run_it=True):
 
 
 def reconfigure_autostart_policy():
+    '''
+    Set the system boot flow depending on which users are available on the system.
+    With no kano users, set things ready to start the Onboarding on boot.
+    With one kano users, set the system to go directly to the Dashboard.
+    With more than once kano user, set the system to go to the login Greeter.
+    '''
     kanousers = get_group_members('kanousers')
     if len(kanousers) == 0:
         enable_console_autologin('root')
@@ -155,7 +153,7 @@ def restore_factory_settings():
     # has side effects that break peldins build
     from kano_settings.default import set_default_config
 
-    # removing wifi cache
+    # remove the wifi cache, effectively avoiding a wireless connection on boot
     try:
         os.remove('/etc/kwifiprompt-cache.conf')
     except Exception:
@@ -165,18 +163,20 @@ def restore_factory_settings():
 
 
 def is_any_task_scheduled():
+    '''
+    Returns True if there is an uncompleted kano-init task
+    '''
     status = Status.get_instance()
     return status.stage != Status.DISABLED_STAGE
 
 
 def load_init_conf():
     """
-        Load the init configuration from the boot partition.
+    Load the init configuration from the boot partition.
 
-        :return: The config in a dictionary.
-        :rtype: dict
+    :return: The config in a dictionary.
+    :rtype: dict
     """
-
     flow_params = {}
     if os.path.exists(INIT_CONF_PATH):
         with open(INIT_CONF_PATH, 'r') as init_conf:
